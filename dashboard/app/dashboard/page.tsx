b3dc78fd05c14bf9f5ce640bb92e730a9844f1eb
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-05-10 22:52:17 +0530

    wrap in the suspense

--- dashboard/app/dashboard/page.tsx
+++ dashboard/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
-"use client";
-
+import { Suspense } from "react";
 import HomeContent from "../HomeContent";
 
 export default function Page() {
-  return <HomeContent />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeContent />
+    </Suspense>
+  );
 }
